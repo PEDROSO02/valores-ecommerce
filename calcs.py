b9ad05843e84d458902a75ac8357332e0fdff2c0
EUPEDROSO02 <guispedroso14@gmail.com>
2026-02-28 14:11:31 -0300

    Atualização Ifood, em desenvolvimento.

--- calcs.py
+++ calcs.py
@@ -7,7 +7,7 @@ tml = 12.5
 valor_produto = float(input("Digite o valor que você paga no produto: "))
 print("Qual sua plataforma de venda?")
 
-plataforma = int(input("Digite 1 para ML e 2 para Shopee"))
+plataforma = int(input("Digite: 1 - Mercado livre \n 2 - Shopee \n 3 - Ifood"))
 lucro = float(input("Qual lucro (em R$) deseja obter nesta venda? "))
 
 if plataforma == 1:
@@ -25,6 +25,25 @@ elif plataforma == 2:
      print(f"A tala cobrada pela shopee será de R$ {taxa:.2f}" )
      print(f"Para obter o lucro desejado, precisará vender o produto a R${pv:.2f}")
      
+elif plataforma == 3:
+    vmes = float(input("Quanto você costuma vender no mês (R$)? "))
+    plano = int(input("Qual plano pretende usar? \n 1 - básico \n 2 - Entrega"))
+    basico = 0.16
+    entrega = 0.26
+    
+    if vmes >= 1800 and plano == 1:
+        lucro = (vmes - 130) * (1 - basico)
+    elif vmes >= 1800 and plano ==2:
+        lucro = (vmes - 150) * (1 - entrega)
+        
+    elif vmes < 1800 and plano ==1:
+        lucro = vmes * (1 - basico)
+    elif vmes < 1800 and plano ==2:
+        lucro = vmes * (1 - entrega)
+        
+        print(f"Usando o plano {plano} e vendendo R${vmes} seu lucro será de {lucro}")
+
+ 
 else:
     print("A opção selecionada não é valida")
     
